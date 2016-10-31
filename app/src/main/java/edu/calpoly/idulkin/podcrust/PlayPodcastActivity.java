076a1653b0f679a486800ed9381f2283f974f809
mrchianglin <jonathanchianglin@live.com>
2016-10-31 00:26:29 -0700

    Weird Lines, MediaPlayer, raw dirc
    Trends work.
    Fixed some weird lines
    Created a MediaPlayer came from TutorialPoint with some buttons
    Created a raw directory to store podcasts temporarily

---
+++ app/src/main/java/edu/calpoly/idulkin/podcrust/PlayPodcastActivity.java
@@ -0,0 +1,13 @@
+package edu.calpoly.idulkin.podcrust;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayPodcastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_podcast);
+    }
+}
