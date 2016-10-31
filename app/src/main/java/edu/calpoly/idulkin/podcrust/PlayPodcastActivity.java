96aa6743898df765f07828ad3cf4bdfa429c4b05
Igor Dulkin <igordulkin@gmail.com>
2016-10-30 22:21:38 -0700

    Ok actually pushing the activity now

---
+++ app/src/main/java/edu/calpoly/idulkin/podcrust/PlayPodcastActivity.java
@@ -0,0 +1,38 @@
+package edu.calpoly.idulkin.podcrust;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class PlayPodcastActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private static final String TAG = PlayPodcastActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_podcast);
+        String url = "http://soundbible.com/grab.php?id=1851&type=mp3";
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare(); // might take long! (for buffering, etc)
+            mediaPlayer.start();
+        } catch(IOException e){
+            Log.e(TAG, "Failed to open media stream");
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+}
