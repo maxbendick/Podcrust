562936bb23b98098bbac93fff29fcb204526db39
mrchianglin <jonathanchianglin@live.com>
2016-11-27 21:49:23 -0800

    Started with fragment but singlePane is 2 activities but no Fragment

--- app/src/main/java/edu/calpoly/idulkin/podcrust/EpisodeDetailFragment.java
+++ app/src/main/java/edu/calpoly/idulkin/podcrust/EpisodeDetailFragment.java
@@ -27,31 +27,39 @@ public class EpisodeDetailFragment extends Fragment {
     /**
      * The dummy content this fragment is presenting.
      */
-    private DummyContent.DummyItem mItem;
+    private String mp3;
+    private String title;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
-    public EpisodeDetailFragment() {
+    public static EpisodeDetailFragment newInstance(String title, String mp3) {
+        EpisodeDetailFragment edf = new EpisodeDetailFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("TITLE", title);
+        bundle.putString("MP3", mp3);
+        edf.setArguments(bundle);
+        return  edf;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
-        }
+//        if (getArguments().containsKey(ARG_ITEM_ID)) {
+//            // Load the dummy content specified by the fragment
+//            // arguments. In a real-world scenario, use a Loader
+//            // to load content from a content provider.
+//            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+//
+//            Activity activity = this.getActivity();
+//            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+//            if (appBarLayout != null) {
+//                appBarLayout.setTitle(mItem.content);
+//            }
+//        }
     }
 
     @Override
@@ -66,4 +74,6 @@ public class EpisodeDetailFragment extends Fragment {
 
         return rootView;
     }
+
+
 }
