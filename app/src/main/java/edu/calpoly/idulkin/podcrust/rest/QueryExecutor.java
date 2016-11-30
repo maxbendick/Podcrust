e4edf6cc9324624f28f804ad75892dfaf92bb642
Max Bendick <maxbendick@gmail.com>
2016-11-29 20:46:32 -0800

    Update spring/rest dependencies to newer versions

--- app/src/main/java/edu/calpoly/idulkin/podcrust/rest/QueryExecutor.java
+++ app/src/main/java/edu/calpoly/idulkin/podcrust/rest/QueryExecutor.java
@@ -2,7 +2,7 @@ package edu.calpoly.idulkin.podcrust.rest;
 
 import android.util.Log;
 
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter ;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class QueryExecutor {
     public static Trending getTrending() {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());;
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter ());;
         Trending t = restTemplate.getForObject("https://www.audiosear.ch/api/trending", Trending.class);
         Log.d("QueryExecutor", t.toString());
         return t;
